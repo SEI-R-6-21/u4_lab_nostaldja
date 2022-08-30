45031c2e0cf06ef969bb9134d77b8f9a0e48418b
Steve Morello <smorello4@gmail.com>
2022-08-30 17:53:46 -0400

    complete mvp

--- nostaldja_app/nostaldja/models.py
+++ nostaldja_app/nostaldja/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Decade(models.Model):
+  name = models.CharField(max_length=100)
+  start_year = models.DateField(auto_now=False,auto_now_add=False)
+  end_year = models.DateField(auto_now=False,auto_now_add=False)
+
+  def __str__(self):
+    return self.name
+
+class Fad(models.Model):
+  name = models.CharField(max_length=100)
+  image_url = models.TextField()
+  description = models.CharField(max_length=100)
+  decade = models.ForeignKey(Decade, on_delete=models.CASCADE, related_name='fads')
+
+  def __str__(self):
+    return self.name
