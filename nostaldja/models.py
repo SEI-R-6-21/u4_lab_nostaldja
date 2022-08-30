a2310246605ff7faa246e82d03d08aad98c8ffbe
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-08-30 17:14:40 -0400

    wip: models created, fixed cascade error

--- nostaldja/models.py
+++ nostaldja/models.py
@@ -1,8 +1,3 @@
-from email.mime import image
-from statistics import mode
-from tkinter import CASCADE
-from tracemalloc import start
-from unicodedata import name
 from django.db import models
 
 # Create your models here.
@@ -17,11 +12,11 @@ class Decade(models.Model):
     def __str__(self):
         return self.name
 
+
 # FAD MODEL
-
-
 class Fad(models.Model):
-    decade = models.ForeignKey(Decade, on_delete=CASCADE, related_name='fads')
+    decade = models.ForeignKey(
+        Decade, on_delete=models.CASCADE, related_name='fads')
     name = models.CharField(max_length=100)
     image_url = models.CharField(max_length=200, null=True)
     description = models.TextField()
