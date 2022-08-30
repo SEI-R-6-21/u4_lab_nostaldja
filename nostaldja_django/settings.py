c0ba5879cb2c50ab76e0501823e1ebc0b52ca0a6
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-08-30 16:21:42 -0400

    wip: connecting app to db

--- nostaldja_django/settings.py
+++ nostaldja_django/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'nostaldja_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nostaldja',
+        'USER': 'nostaldjauser',
+        'PASSWORD': 'nostalgia',
+        'HOST': 'localhost'
     }
 }
 
